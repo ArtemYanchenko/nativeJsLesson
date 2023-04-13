7bd87f29e0dd2a1a9e846a9aa4cc486354b11021
Artem Yanchenko <artem.yanchenko.94@mail.ru>
2023-04-13 20:09:39 +0300

    done lesson 7

--- main-07.js
+++ main-07.js
@@ -5,15 +5,14 @@
 // repeatString("yo", 0, ", ") => ""
 // repeatString("yo", 1, ", ") => "yo"
 
-function repeatString (str,count,rep){
+function repeatString (str,count,sep){
     let newStr ;
 
     for (let i = 0; i <= count; i++) {
-        newStr = (str + rep).repeat(i)
+        newStr = (str + sep).repeat(i)
     }
-    return newStr.slice(0, -rep.length);
+    return newStr.slice(0, -sep.length);
 }
-
 console.log(repeatString('yo',3,','))
 
 
@@ -25,11 +24,15 @@ console.log(repeatString('yo',3,','))
 function checkStart (str,subStr) {
     return str.toLowerCase().includes(subStr.toLowerCase())
 }
-
 console.log(checkStart("Incubator", "inc"))
 
 //3. Реализуйте функцию, которая принимает параметром строку и число (количество символов), а возвращает строку из параметров, обрезанную до указанного количества символов и завершает её многоточием.
 //truncateString("Всем студентам инкубатора желаю удачи!", 10) => "Всем студе..."
+
+function truncateString (string,count) {
+    return string.slice(0,count) + '...'
+}
+console.log(truncateString("Всем студентам инкубатора желаю удачи!", 10))
 
 //4. Реализуйте функцию, которая принимает параметром сторку (предложение) и возвращает самое короткое слово в предложении, если в параметрах пустая строка или не строка, то возвращает null.
 // getMinLengthWord("Всем студентам инкубатора желаю удачи.") => "Всем"
@@ -38,9 +41,24 @@ console.log(checkStart("Incubator", "inc"))
 // getMinLengthWord(true) => null
 // getMinLengthWord(undefined) => null
 
+
+function getMinLengthWord (string) {
+    if(typeof(string) === 'string' && string.length !== 0) {
+    return string.split(' ').sort((a,b)=>a.length-b.length)[0]
+        } else return null
+}
+console.log(getMinLengthWord("Всем студентам инкубатора желаю удачи."))
+
+
 //5. Реализуйте функцию, которая принимает параметром сторку (предложение) и возвращает то же предложение, где все слова написаны строчными, но начинаются с заглавных букв.
 // setUpperCase("всем стУдентам инкуБатора Желаю удачИ") => "Всем Студентам Инкубатора Желаю Удачи!"
 
+function  setUpperCase (string) {
+    const first = string.toLowerCase().split(' ').map(el=>el[0].toUpperCase() + el.substring(1)).join(' ');
+    return first
+
+}
+console.log(setUpperCase("всем стУдентам инкуБатора Желаю удачИ"))
 // !!!!!!!!!!!!!!!!!!После решения 5 задач - поднимаем руку!!!!!!!!
 
 //6. Реализуйте функцию, котрая принимает параметрами строку и подстроку. Если все
